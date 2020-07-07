104230690fe32c63514d41a80a9a786a9f4e36d7
Victor Lima <victorfrancovl@gmail.com>
2020-07-07 12:42:17 -0300

    Adiciona tratamento de erros

--- src/graphics/loader.cc
+++ src/graphics/loader.cc
@@ -11,7 +11,8 @@ std::unique_ptr<Image> loadImage(const char *filename) {
 
   const auto error = lodepng::decode(buffer, width, height, filename);
   if (error) {
-    //
+    throw std::runtime_error(
+      std::string{"Falha carregando imagem: "} + lodepng_error_text(error));
   }
 
   auto image = std::make_unique<Image>(width, height);
@@ -48,7 +49,10 @@ void writeImage(const char *filename, const Image &image) {
   }
 
   const auto error = lodepng::encode(filename, buffer, width, height);
-  // todo
+  if (error) {
+    throw std::runtime_error(
+      std::string{"Falha salvando imagem: "} + lodepng_error_text(error));
+  }
 }
 
 }  // namespace graphics
